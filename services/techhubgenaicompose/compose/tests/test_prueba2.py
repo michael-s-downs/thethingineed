7bbcdd382b6afec18583694e2da67ab2b5c8a886
USERSAD\idegrego <idegrego@emeal.nttdata.com>
2024-10-08 15:28:36 +0200

    [compose] test init

---
+++ services/techhubgenaicompose/compose/tests/test_prueba2.py
@@ -0,0 +1,5 @@
+import os
+from compose import streambatch
+
+def test_prueba2():
+    assert True == True
---
+++ services/techhubgenaicompose/tests/test_prueba1.py
@@ -0,0 +1,6 @@
+from actionsmanager import ActionsManager
+
+
+def test_hola():
+    x = ActionsManager(None, None)
+    assert True==True
