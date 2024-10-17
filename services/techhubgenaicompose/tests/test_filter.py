46867bfef95561b9e220556f69d3eb8ddf7b789d
USERSAD\erozasqu <erozasqu@emeal.nttdata.com>
2024-10-17 17:44:05 +0200

    [compose] Updated test filter

--- services/techhubgenaicompose/tests/test_filter.py
+++ services/techhubgenaicompose/tests/test_filter.py
@@ -1,17 +1,93 @@
 ### This code is property of the GGAO ###
 
 import pytest
-from compose.actions.filter import TopKFilter, MetadataFilter, FilterFactory
+from compose.actions.filter import (
+    FilterMethod,
+    TopKFilter,
+    PermissionFilter,
+    RelatedToFilter,
+    MetadataFilter,
+    FilterFactory,
+)
+import json
+from common.errors.genaierrors import PrintableGenaiError
 from datetime import datetime
+
+# import asyncio
+from unittest.mock import AsyncMock, patch, MagicMock
+from aioresponses import aioresponses
+
+# from compose.streamchunk import StreamChunk
+from copy import deepcopy
+# import aiohttp
+
+
+class DummyFilter(FilterMethod):
+    def process(self):
+        """Implementación del método process de FilterMethod."""
+        return super().process()
+
+    def _get_example(self):
+        """Implementación del método _get_example de FilterMethod."""
+        return super()._get_example()
+
+
+@pytest.fixture
+def dummy_filter():
+    """Fixture para crear una instancia de DummyFilter."""
+    return DummyFilter(streamlist=["x", "y", "z"])
+
+
+@pytest.fixture
+def top_k_filter():
+    """Fixture para crear una instancia de TopKFilter con una lista de ejemplo."""
+    streamlist = ["a", "b", "c", "d", "e"]
+    return TopKFilter(streamlist)
+
+
+'''
+@pytest.fixture
+def permission_filter():
+    """Fixture para crear una instancia de PermissionFilter con un streamlist de ejemplo."""
+    streamlist = [
+        {"meta": {"knowler_id": "1"}},
+        {"meta": {"knowler_id": "2"}},
+        {"meta": {"knowler_id": "3"}},
+    ]
+    return PermissionFilter(streamlist)
+'''
+
+
+@pytest.fixture
+def metadata_filter():
+    """Fixture para crear una instancia de MetadataFilter con una lista de ejemplo."""
+    streamlist = [{"meta": {"some_key": "some_value"}}]
+    return MetadataFilter(streamlist)
+
+
+class TestFilterMethod:
+    def test_process_abstract_method(self, dummy_filter):
+        """Test para cubrir el pass en el método abstracto process."""
+        result = dummy_filter.process()
+        assert result is None
+
+    def test_get_example(self, dummy_filter):
+        """Test que cubre el método get_example de FilterMethod."""
+        result = dummy_filter.get_example()
+        expected_result = json.dumps({})
+        assert result == expected_result
+
+    def test_get_example_with_super(self, dummy_filter):
+        """Test para cubrir el return {} en _get_example usando super()."""
+        assert dummy_filter._get_example() == {}
+
+    def test_get_example_json(self, dummy_filter):
+        """Test para asegurar que el método get_example llame correctamente a _get_example."""
+        result = dummy_filter.get_example()
+        assert result == json.dumps({})
 
 
 class TestTopKFilter:
-    @pytest.fixture
-    def top_k_filter(self):
-        """Fixture to create a TopKFilter instance with a sample streamlist."""
-        streamlist = ["a", "b", "c", "d", "e"]
-        return TopKFilter(streamlist)
-
     def test_process_valid_top_k(self, top_k_filter):
         """Test processing with valid top_k parameter."""
         result = top_k_filter.process({"top_k": 3})
@@ -22,43 +98,10 @@ class TestTopKFilter:
         with pytest.raises(ValueError):
             top_k_filter.process({"top_k": "invalid"})
 
-    # def test_process_no_top_k(self, top_k_filter):
-    #    """Test processing without top_k parameter."""
-    #   result = top_k_filter.process({})
-    #    assert result == []
-
     def test_get_example(self, top_k_filter):
         """Test the _get_example method."""
         expected_example = {"type": "top_k", "params": {"top_k": 5}}
         assert top_k_filter._get_example() == expected_example
-
-
-# class TestPermissionFilter:
-#   @pytest.fixture
-#    def permission_filter(self):
-#        """Fixture to create a PermissionFilter instance with a sample streamlist."""
-#        streamlist = [
-#            {"meta": {"knowler_id": "doc1"}},
-#            {"meta": {"knowler_id": "doc2"}},
-#        ]
-#        return PermissionFilter(streamlist)
-
-#   @pytest.mark.asyncio
-#    async def test_process_valid_permissions(self, permission_filter, mocker):
-#        """Test processing with valid permissions."""
-#        mock_response = {"allowed": {"doc1": True, "doc2": False}}
-#        mocker.patch.object(
-#            permission_filter, "send_post_request", return_value=mock_response
-#        )
-
-#       params = {
-#            "url_allowed_documents": "http://ejemplo.com",
-#            "headers_config": {"user-token": "fake-token"},
-#        }
-#        result = await permission_filter.process(params)
-
-#       assert len(result) == 1
-#        assert result[0]["meta"]["knowler_id"] == "doc1"
 
 
 class MockStreamChunk:
@@ -190,6 +233,96 @@ class TestMetadataFilter:
         }
         assert example == expected
 
+    def test_apply_subfilter_key_not_exist(self, metadata_filter):
+        """Test apply_subfilter raises PrintableGenaiError with non-existent key."""
+        non_existent_key = {"invalid_key": ("invalid_key", "some_value")}
+
+        with pytest.raises(PrintableGenaiError) as exc_info:
+            metadata_filter.apply_subfilter({"city": "New York"}, non_existent_key)
+
+        assert exc_info.value.status_code == 404
+        assert "Metadata key invalid_key does not exist" in str(exc_info.value)
+
+    def test_apply_subfilter_in(self, metadata_filter):
+        """Test apply_subfilter with 'in' condition."""
+
+        result = metadata_filter.apply_subfilter(
+            {"city": "New York"}, {"in": ("city", ["New York", "London"])}
+        )
+        assert result is True
+
+        result = metadata_filter.apply_subfilter(
+            {"city": "Paris"}, {"in": ("city", ["New York", "London"])}
+        )
+        assert result is False
+
+    def test_apply_subfilter_textinmeta(self, metadata_filter):
+        """Test apply_subfilter with 'textinmeta' condition."""
+        metadata = {"description": "The city of New York is known as the big apple."}
+        sub_filter = {"textinmeta": ("description", "big apple")}
+
+        result = metadata_filter.apply_subfilter(metadata, sub_filter)
+
+        assert result is True
+
+    '''
+    def test_apply_subfilter_metaintext(self, metadata_filter):
+        """Test apply_subfilter with 'metaintext' condition."""
+        metadata = {"city": "New York"}
+        sub_filter = {
+            "metaintext": ("city", ["New York is a city", "London is a city"])
+        }
+
+        # Asegúrate de que este resultado sea verdadero
+        result = metadata_filter.apply_subfilter(metadata, sub_filter)
+        assert result is True
+    '''
+
+    def test_apply_subfilter_eq_date(self, metadata_filter):
+        """Test apply_subfilter with 'eq_date' condition."""
+
+        metadata = {"event_date": "2024-10-17"}
+
+        sub_filter = {"eq_date": ("event_date", "2024-10-17")}
+
+        result = metadata_filter.apply_subfilter(metadata, sub_filter)
+
+        assert result is True
+
+    def test_apply_subfilter_gt_date(self, metadata_filter):
+        """Test apply_subfilter with 'gt_date' condition."""
+
+        metadata = {"event_date": "2024-10-18"}
+
+        sub_filter = {"gt_date": ("event_date", "2024-10-17")}
+
+        result = metadata_filter.apply_subfilter(metadata, sub_filter)
+
+        assert result is True
+
+    def test_apply_subfilter_lt_date(self, metadata_filter):
+        """Test apply_subfilter with 'lt_date' condition."""
+
+        metadata = {"event_date": "2024-10-16"}
+
+        sub_filter = {"lt_date": ("event_date", "2024-10-17")}
+
+        result = metadata_filter.apply_subfilter(metadata, sub_filter)
+
+        assert result is True
+
+    def test_process_with_no_keys(self, metadata_filter):
+        """Test process raises an error when no filter conditions are provided."""
+        params = {
+            "filter_conditions": {},
+            "operator": "and",
+        }
+        with pytest.raises(PrintableGenaiError) as exc_info:
+            metadata_filter.process(params)
+
+        assert exc_info.value.status_code == 500
+        assert "Only 'and' or 'or' keys must be defined." in str(exc_info.value)
+
 
 class TestFilterFactory:
     @pytest.fixture
@@ -220,3 +353,130 @@ class TestFilterFactory:
         with pytest.raises(Exception) as excinfo:
             factory.process(streamlist, {"top_k": 0})
         assert "NO documents passed the filters" in str(excinfo.value)
+
+
+@pytest.fixture
+def permission_filter():
+    streamlist = [
+        MagicMock(meta={"knowler_id": "1"}),
+        MagicMock(meta={"knowler_id": "2"}),
+        MagicMock(meta={"knowler_id": "3"}),
+    ]
+
+    return PermissionFilter(streamlist)
+
+
+@pytest.mark.asyncio
+class TestPermissionFilter:
+    @pytest.fixture(autouse=True)
+    def setup_method(self, monkeypatch):
+        monkeypatch.setenv("URL_ALLOWED_DOCUMENTS_KNOWLER", "http://example.com")
+
+    async def mock_send_post_request(self, url, body, headers):
+        """Simula la respuesta de la función asíncrona send_post_request."""
+        return {"allowed": {"1": True, "2": False, "3": True}}
+
+    def test_update_params(self, permission_filter):
+        """Test para el método update_params."""
+        params = {
+            "headers_config": {"Authorization": "Bearer token"},
+            "additional_param": "value",
+        }
+        headers, template = permission_filter.update_params(params)
+
+        assert headers["Authorization"] == "Bearer token"
+        assert template["additional_param"] == "value"
+
+    def test_get_example(self, permission_filter):
+        """Test que verifica que get_example retorna el formato correcto."""
+        expected_example = {
+            "type": permission_filter.TYPE,
+            "params": permission_filter.TEMPLATE,
+        }
+
+        result = permission_filter.get_example()
+        assert json.loads(result) == expected_example
+
+    def test_get_example_structure(self, permission_filter):
+        """Test que verifica la estructura del ejemplo retornado."""
+        example = permission_filter._get_example()
+        assert "type" in example
+        assert "params" in example
+        assert example["type"] == permission_filter.TYPE
+        assert example["params"] == permission_filter.TEMPLATE
+
+    def test_permission_filter_url_none(self, permission_filter):
+        """Test para cubrir el caso donde URL es None."""
+        permission_filter.URL = None
+
+        with pytest.raises(PrintableGenaiError) as exc_info:
+            permission_filter.process({})
+
+        assert exc_info.value.status_code == 400
+
+        assert "Variable URL_ALLOWED_DOCUMENT not found" in str(exc_info.value)
+
+    async def test_send_post_request_success(self, permission_filter):
+        """Test para send_post_request cuando la respuesta es exitosa."""
+        url = "http://example.com"
+        body = {"internalIds": ["1", "2", "3"]}
+        headers = {"Authorization": "Bearer token"}
+
+        with aioresponses() as m:
+            m.post(url, payload={"allowed": {"1": True, "2": False, "3": True}})
+            response = await permission_filter.send_post_request(url, body, headers)
+
+            assert response == {"allowed": {"1": True, "2": False, "3": True}}
+
+    async def test_send_post_request_failure(self, permission_filter):
+        """Test para send_post_request cuando la respuesta falla."""
+        url = "http://example.com"
+        body = {"internalIds": ["1", "2", "3"]}
+        headers = {"Authorization": "Bearer token"}
+
+        with aioresponses() as m:
+            m.post(url, status=400, payload="Bad Request")
+            with pytest.raises(PrintableGenaiError) as exc_info:
+                await permission_filter.send_post_request(url, body, headers)
+
+            assert exc_info.value.status_code == 400
+            assert "Bad Request" in str(exc_info.value)
+
+
+class TestRelatedToFilter:
+    @pytest.fixture
+    def filter_instance(self):
+        streamlist = []
+        return RelatedToFilter(streamlist)
+
+    def test_update_params_without_headers_config(self, filter_instance):
+        params = {"param1": "value1", "param2": "value2"}
+        expected_headers = deepcopy(filter_instance.HEADERS)
+        expected_template = deepcopy(filter_instance.TEMPLATE)
+        expected_template.update(params)
+
+        headers, template = filter_instance.update_params(params)
+
+        assert headers == expected_headers
+        assert template == expected_template
+
+    def test_update_params_empty_params(self, filter_instance):
+        params = {}
+        expected_headers = deepcopy(filter_instance.HEADERS)
+        expected_template = deepcopy(filter_instance.TEMPLATE)
+
+        headers, template = filter_instance.update_params(params)
+
+        assert headers == expected_headers
+        assert template == expected_template
+
+    def test_update_params_with_missing_key(self, filter_instance):
+        params = {"headers_config": {"Authorization": "Bearer token"}}
+        expected_headers = deepcopy(filter_instance.HEADERS)
+        expected_headers.update(params["headers_config"])
+        expected_template = deepcopy(filter_instance.TEMPLATE)
+
+        headers, template = filter_instance.update_params(params)
+
+        assert headers == expected_headers
+        assert template == expected_template
