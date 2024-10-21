7487e434916009bc1a57aba7a02dd18298bb7ca5
USERSAD\erozasqu <erozasqu@emeal.nttdata.com>
2024-10-21 17:53:13 +0200

    [compose] Test updates llm_action

--- services/techhubgenaicompose/tests/test_llm_action.py
+++ services/techhubgenaicompose/tests/test_llm_action.py
@@ -5,16 +5,18 @@ from compose.actions.llm_action import (
     LLMMethod,
     LLMSummarize,
     # LLMSummarizeContent,
-    # LLMSummarizeAnswer,
-    # LLMSummarizeSegments,
-    # LLMFactory,
+    LLMSummarizeAnswer,
+    LLMSummarizeSegments,
+    LLMFactory,
+    # LLMP,
 )
 
-from unittest.mock import patch, Mock, MagicMock
+from unittest.mock import patch, Mock, MagicMock, AsyncMock
 
 # from compose.streamchunk import StreamChunk
 from common.errors.genaierrors import PrintableGenaiError
-# import json
+import json
+import aiohttp
 
 
 @pytest.fixture
@@ -63,6 +65,10 @@ class DummyLLMMethod(LLMMethod):
     def get_example(self) -> dict:
         """Devuelve un ejemplo de diccionario."""
         return {"type": self.TYPE, "params": self.TEMPLATE}
+
+    def get_example_json(self) -> str:
+        """Devuelve un ejemplo como string JSON."""
+        return json.dumps(self.get_example())
 
 
 class TestLLMMethod:
@@ -271,3 +277,207 @@ class TestLLMSummarize:
 
         assert "persistence" in result_template["query_metadata"]
         assert len(result_template["query_metadata"]["persistence"]) > 0
+
+    def test_add_hystoric_with_system(mock_streamlist, mock_PD, mock_params):
+        llm_summarize = LLMSummarize(mock_streamlist)
+        session_id = "session123"
+
+        mock_PD.get_conversation.return_value = [
+            {"user": "Hello, how are you?", "system": "I'm fine, thank you."},
+            {"assistant": "Glad to hear that!"},
+        ]
+
+        template = {"query_metadata": {}, "PD": mock_PD}
+
+        result_template = llm_summarize.add_hystoric(session_id, template, mock_PD)
+
+        assert "persistence" in result_template["query_metadata"]
+        assert len(result_template["query_metadata"]["persistence"]) > 0
+        assert len(result_template["query_metadata"]["persistence"]) == 1
+        assert result_template["query_metadata"]["persistence"][0][0]["role"] == "user"
+        assert (
+            result_template["query_metadata"]["persistence"][0][0]["content"]
+            == "Hello, how are you?"
+        )
+        assert (
+            result_template["query_metadata"]["persistence"][0][1]["role"] == "system"
+        )
+        assert (
+            result_template["query_metadata"]["persistence"][0][1]["content"]
+            == "I'm fine, thank you."
+        )
+
+    def test_process_no_texts(self, mock_streamlist, mock_PD, mock_params):
+        """Test para el caso donde no hay textos para sumarizar."""
+        llm_summarize = LLMSummarize(mock_streamlist)
+        mock_streamlist.clear()
+        mock_params["query_type"] = "text"
+        template = {
+            "query_metadata": {},
+            "PD": mock_PD,
+            "query_type": "text",
+            "llm_action": [],
+        }
+        session_id = "session123"
+
+        with patch.object(
+            llm_summarize,
+            "call_llm",
+            return_value={
+                "answer": "Summary",
+                "input_tokens": 50,
+                "output_tokens": 30,
+                "query_tokens": [10],
+            },
+        ):
+            result_streamlist = llm_summarize.process(mock_params)
+
+        assert len(result_streamlist) == 0
+
+    def test_process_error_in_llm(self, mock_streamlist, mock_PD, mock_params):
+        """Test para el caso donde ocurre un error al llamar a LLM."""
+        llm_summarize = LLMSummarize(mock_streamlist)
+        mock_params["query_type"] = "text"
+        template = {
+            "query_metadata": {},
+            "PD": mock_PD,
+            "query_type": "text",
+            "llm_action": [],
+        }
+        session_id = "session123"
+
+        with patch.object(
+            llm_summarize, "call_llm", side_effect=PrintableGenaiError(500, "Error")
+        ):
+            with pytest.raises(PrintableGenaiError) as excinfo:
+                llm_summarize.process(mock_params)
+
+        assert excinfo.value.status_code == 500
+
+
+class TestLLMSummarizeAnswer:
+    @pytest.fixture
+    def mock_streamlist_with_answers(self):
+        """Fixture para crear un mock streamlist con respuestas."""
+        return [
+            {"content": "Some content", "answer": "This is an answer."},
+            {"content": "Another content", "answer": "This is another answer."},
+        ]
+
+    def test_clear_output(self, mock_streamlist_with_answers):
+        """Test para asegurar que clear_output elimina las respuestas de streamlist."""
+        llm_summarize_answer = LLMSummarizeAnswer(mock_streamlist_with_answers)
+
+        llm_summarize_answer.clear_output()
+
+        for sl in llm_summarize_answer.streamlist:
+            assert "answer" not in sl
+
+
+class MockLLMSummarizeContent:
+    TYPE = "llm_content"
+
+    def __call__(self, streamlist):
+        return self
+
+    def process(self, params):
+        return "Processed with llm_content"
+
+
+class TestLLMSummarizeSegments:
+    @pytest.mark.asyncio
+    async def test_async_call_llm(mock_streamlist):
+        llm_summarize = LLMSummarizeSegments(mock_streamlist)
+
+        template = {"query_metadata": {}}
+        headers = {"Authorization": "Bearer token"}
+
+        mock_response = AsyncMock()
+        mock_response.json.return_value = {"result": "mocked_result"}
+        mock_response.status = 200
+        mock_response.__aenter__.return_value = mock_response
+        mock_response.__aexit__.return_value = False
+
+        with patch(
+            "aiohttp.ClientSession.post", return_value=mock_response
+        ) as mock_post:
+            async with aiohttp.ClientSession() as session:
+                result = await llm_summarize.async_call_llm(template, headers, session)
+
+                assert result == "mocked_result"
+
+                mock_post.assert_called_once_with(
+                    llm_summarize.URL, json=template, headers=headers, verify_ssl=False
+                )
+
+    @pytest.mark.asyncio
+    async def test_parallel_calls(mock_streamlist):
+        llm_summarize = LLMSummarizeSegments(mock_streamlist)
+
+        templates = [
+            {"query_metadata": {"context": "Content 1"}},
+            {"query_metadata": {"context": "Content 2"}},
+        ]
+        headers = {"Authorization": "Bearer token"}
+
+        with patch.object(
+            llm_summarize,
+            "async_call_llm",
+            side_effect=[{"result": "mocked_result_1"}, {"result": "mocked_result_2"}],
+        ):
+            with patch("aiohttp.ClientSession") as mock_session:
+                mock_session.return_value.__aenter__.return_value = AsyncMock()
+                responses = await llm_summarize.parallel_calls(templates, headers)
+
+                assert responses[0]["result"] == "mocked_result_1"
+                assert responses[1]["result"] == "mocked_result_2"
+
+                assert llm_summarize.async_call_llm.call_count == 2
+                llm_summarize.async_call_llm.assert_any_call(
+                    templates[0],
+                    headers,
+                    mock_session.return_value.__aenter__.return_value,
+                )
+                llm_summarize.async_call_llm.assert_any_call(
+                    templates[1],
+                    headers,
+                    mock_session.return_value.__aenter__.return_value,
+                )
+
+
+class TestLLMFactory:
+    @pytest.fixture
+    def valid_llm_action(self):
+        return "llm_content"
+
+    @pytest.fixture
+    def invalid_llm_action(self):
+        return "invalid_action"
+
+    @pytest.fixture
+    def valid_streamlist(self):
+        return []
+
+    @pytest.fixture
+    def valid_params(self):
+        return {
+            "param1": "value1",
+            "param2": "value2",
+        }
+
+    def test_llm_factory_initialization_valid(self, valid_llm_action):
+        factory = LLMFactory(valid_llm_action)
+        assert factory is not None
+
+    def test_process_success(self, valid_llm_action, valid_streamlist, valid_params):
+        LLMFactory.SUMMARIES = [MockLLMSummarizeContent()]
+        factory = LLMFactory(valid_llm_action)
+        response = factory.process(valid_streamlist, valid_params)
+        assert response == "Processed with llm_content"
+
+    def test_llm_factory_initialization_invalid(self, invalid_llm_action):
+        with pytest.raises(
+            PrintableGenaiError,
+            match="Provided llm_action does not match any of the possible ones",
+        ):
+            LLMFactory(invalid_llm_action)
