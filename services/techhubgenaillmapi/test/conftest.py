928111c3a8ee9850a0721294785ffdd4feb5f0e4
USERSAD\idegrego <idegrego@emeal.nttdata.com>
2024-11-28 10:25:23 +0100

    [llmapi] Quit conf not necessary

--- services/techhubgenaillmapi/test/conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from unittest.mock import MagicMock, patch
-
-@pytest.fixture(autouse=True)
-def patch_load_secrets():
-    with patch("common.utils.load_secrets") as mock_function:
-        mock_function.return_value = MagicMock()
-        yield mock_function
