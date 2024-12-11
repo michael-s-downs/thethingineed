bffcc1b11f50fadaa918569fb482de89223c59d4
Kisco <jpuertoj@emeal.nttdata.com>
2024-12-11 12:53:43 +0100

    Update creation of Redis

--- IaC/Terraform/AWS/Modules/RedisCache/main.tf
+++ IaC/Terraform/AWS/Modules/RedisCache/main.tf
@@ -6,10 +6,10 @@ resource "random_password" "mysql_password" {
 resource "aws_elasticache_cluster" "redis" {
   cluster_id               = "redis-${var.rg}"
   engine                   = "redis"
-  engine_version           = "8.0"
+  engine_version           = "5.0.6"
   node_type                = "cache.t3.micro"
   num_cache_nodes          = 1
-  parameter_group_name     = "default.redis6.x"
+  parameter_group_name     = "default.redis5.0"
   security_group_ids       = [var.sg]
   snapshot_retention_limit = 0
   subnet_group_name        = var.subnet_group
