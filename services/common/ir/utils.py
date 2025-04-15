890b6b548d46dfd9c9ca849942da1bd04e3a32d7
USERSAD\idegrego <idegrego@emeal.nttdata.com>
2025-04-15 13:05:52 +0200

    [ir] EMBED MODEL not call every time

--- services/common/ir/utils.py
+++ services/common/ir/utils.py
@@ -17,6 +17,7 @@ from common.ir.connectors import Connector, ManagerConnector
 
 IR_INDICES = "src/ir/index/"
 INDEX_STORAGE = lambda index: IR_INDICES + index + ".json"
+EMBED_MODEL = None
 
 
 
@@ -77,9 +78,12 @@ def get_embed_model(model: dict, aws_credentials: dict, is_retrieval: bool) -> B
                 model_name=model.get('embedding_model')
             )
     elif platform == "huggingface":
-        if is_retrieval:
-            # Normally in huggingface the retrieval model is different from the embedding model
-            return HuggingFaceEmbedding(model_name=model.get('retriever_model'), trust_remote_code=True )
-        return HuggingFaceEmbedding(model_name=model.get('embedding_model'), trust_remote_code=True)
+        if EMBED_MODEL is None:
+            if is_retrieval:
+                # Normally in huggingface the retrieval model is different from the embedding model
+                EMBED_MODEL = HuggingFaceEmbedding(model_name=model.get('retriever_model'), trust_remote_code=True )
+            EMBED_MODEL = HuggingFaceEmbedding(model_name=model.get('embedding_model'), trust_remote_code=True)
+
+        return EMBED_MODEL
     else:
         raise ValueError(f"Platform {platform} not supported")
