8b1c1fb893ceaffe81fe7d68ef7db1f900f8da07
USERSAD\aarteahe <aarteahe@emeal.nttdata.com>
2025-05-29 16:46:32 +0200

    [LLMAPI] test tsuzumi implementation

--- services/techhubgenaillmapi/test/test_messages.py
+++ services/techhubgenaillmapi/test/test_messages.py
@@ -17,6 +17,7 @@ from message.claudemessage import ClaudeMessage, Claude3Message
 from message.gptmessage import ChatGPTvMessage, ChatGPTMessage, DalleMessage, ChatGPTOMiniMessage
 from message.geminimessage import GeminiVMessage
 from message.messagemanager import ManagerMessages
+from message.tsuzumimessage import TsuzumiMessage
 
 no_vision_persistence = [
         [
@@ -76,9 +77,11 @@ class TestManagerMessages:
     def test_get_possible_messages(self):
         messages = ManagerMessages.get_possible_messages()
         print(messages)
-        assert messages == ['chatGPT', 'chatClaude', 'dalle', 'chatClaude-v', 'chatGPT-v', 'chatGPT-o1-mini', 'chatLlama3', 'chatNova', 'chatNova-v', 'chatGemini-v']
+        assert messages == ['chatGPT', 'chatClaude', 'dalle', 'chatClaude-v', 'chatGPT-v', 'chatGPT-o1-mini', 'chatLlama3', 'chatNova', 'chatNova-v', 'chatGemini-v', "chatTsuzumi"]
 
     def test_all_messages(self):
+        self.conf['message'] = "chatTsuzumi"
+        message_tsuzumi = ManagerMessages.get_message(self.conf)
         self.conf['message'] = "chatGPT-v"
         message_gptv = ManagerMessages.get_message(self.conf)
         self.conf['message'] = "chatClaude"
@@ -96,6 +99,7 @@ class TestManagerMessages:
         self.conf['message'] = "chatGemini-v"
         message_geminiv = ManagerMessages.get_message(self.conf)
 
+        assert isinstance(message_tsuzumi, TsuzumiMessage)
         assert isinstance(message_gptv, ChatGPTvMessage)
         assert isinstance(message_claude, ClaudeMessage)
         assert isinstance(message_claude3, Claude3Message)
@@ -240,3 +244,22 @@ class TestGeminiMessage:
         message['context'] = "context"
         with pytest.raises(PrintableGenaiError, match="Error 400: Context param not allowed in vision models"):
             GeminiVMessage(**message)
+
+class TestTsuzumiMessage:
+    def test_system(self):
+        message = copy.deepcopy(message_dict)
+        message['system'] = "You are a helpful assistant."
+        message['template'].pop('system')
+        message_obj = ChatGPTMessage(**message)
+        assert isinstance(message_obj, ChatGPTMessage)
+
+    def test_repr_method(self):
+        message = copy.deepcopy(message_dict)
+        message['template_name'] = "system_query"
+        message['context'] = "jasdlkfjañ"
+        message.pop('persistence')
+        message_obj = ChatGPTMessage(**message)
+
+        expected_output = (f"{{query:{message['query']}, context:{message['context']}, "
+                           f"template_name:{message['template_name']}, template:{message['template']}}}")
+        assert repr(message_obj) == expected_output
